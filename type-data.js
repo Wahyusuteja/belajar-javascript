fa2d674a86a445a3737bfc3f3bc15b9f9d9a0947
Wahyusuteja <wahyusuteja688@gmail.com>
2023-11-18 17:59:40 +0700

    belajar type data

---
+++ type-data.js
@@ -0,0 +1,16 @@
+// ini buat cek type data, dengan perintah typeof
+const umur = 20;
+console.log("type data umur adalah:", typeof umur, umur);
+
+let namaBuah = "Peapaya";
+namaBuah = "Anggur";
+console.log("type data nama buah",typeof namaBuah, namaBuah);
+
+var bolean = true;
+console.log("type data bolean", typeof bolean,bolean);
+
+var object = {"nama":"wahyu","umur":20};
+console.log("type data object adalah", typeof object, object);
+
+var array = [{"nama":"wahyu","umur":20}];
+console.log("type data array adalah", typeof array, array[0].nama);
