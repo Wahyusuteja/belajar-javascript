c0ae53c6e206121c82f4960c2576530e9ea19a9d
Wahyusuteja <wahyusuteja688@gmail.com>
2023-11-19 22:06:25 +0700

    belajar array

--- tugas-array.js
+++ tugas-array.js
@@ -4,3 +4,29 @@ console.log(Fruit[1]);
 console.log(Fruit[2]);
 console.log(Fruit[3]);
 console.log(Fruit[4]);
+
+let siswa = [
+{
+ "perempuan": {
+    "nama" : "putri",
+    "umur" : "17",
+    "nilai" : "90",
+    },
+},
+{
+    "pria" : {
+    "nama" : "samsul",
+    "umur" : "18",
+    "nilai" : "80",
+},
+}
+];
+
+console.log(siswa[0].perempuan.nama)
+console.log(siswa[0].perempuan.umur)
+console.log(siswa[0].perempuan.nilai)
+console.log(siswa[1].pria.nama);
+for (var i = 0; i < siswa.length; i++) {
+    console.log(siswa,{i})
+}
+console.log(siswa[siswa.length-3])
