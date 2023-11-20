ad380eb142df23b202f764895f4e58cad4c7a8b2
Wahyusuteja <wahyusuteja688@gmail.com>
2023-11-20 20:18:47 +0700

    belajar if di dalam loop

---
+++ tugas-1.js
@@ -0,0 +1,11 @@
+let animals = ["Lion","Elephant","Tiger","Girafe","Monkey"];
+let hewan;
+
+animals.forEach(function(item){
+    if (item.toUpperCase()==="GIRAFE")
+    hewan = item
+});
+// if (item === "Tiger")
+    // hewan = item
+// });
+console.log("binatang apa",hewan);
